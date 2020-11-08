1ccb3c50500f01a5152e431674c289f0a07f5237
Henrique Macedo <mail@henriquemacedo.pt>
2020-11-08 20:06:44 +0000

    Add style to navbar menu
    - rename globals.css to global.css
    - add n2 logo image
    - add style on navbar menu

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,88 @@
 import Link from "next/link";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+
+const Wrapper = styled.div`
+  background-color: var(--primary);
+  padding: 2vw 5vw;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+
+  img {
+    width: auto;
+    min-height: 80px;
+    height: 6vw;
+    max-height: 150px;
+  }
+
+  ul {
+    --margin: 2vw;
+
+    margin: 0 0 0 var(--margin);
+    padding: 0;
+    display: flex;
+
+    li {
+      list-style: none;
+      position: relative;
+      text-transform: uppercase;
+      font-weight: var(--extraBold);
+
+      &:not(:last-child) {
+        margin-right: var(--margin);
+      }
+
+      &.active:after {
+        content: "";
+        position: absolute;
+        bottom: -5px;
+        left: 10px;
+        width: 100%;
+        height: 5px;
+        background-color: var(--secondary);
+      }
+    }
+  }
+`;
 
 export default function Header() {
+  const router = useRouter();
+
+  const menu = [
+    {
+      label: "Início",
+      url: "/",
+    },
+    {
+      label: "Episódios",
+      url: "/episodios",
+    },
+    {
+      label: "Sobre",
+      url: "/sobre",
+    },
+  ];
+
   return (
     <>
-      <header className="header">
-        <nav className="nav">
-          <Link href="/">
-            <a>Início</a>
-          </Link>{" "}
-          -{" "}
-          <Link href="/episodios">
-            <a>Episódios</a>
-          </Link>{" "}
-          -{" "}
-          <Link href="/sobre">
-            <a>Sobre</a>
-          </Link>
-        </nav>
-      </header>
+      <Wrapper className="header">
+        <Nav>
+          <img src="/n2-logo.svg" alt="Nacional 2" />
+          <ul>
+            {menu.map((item, index) => (
+              <li
+                key={index}
+                className={router.pathname == item.url ? "active" : ""}
+              >
+                <Link href={item.url}>{item.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </Nav>
+      </Wrapper>
     </>
   );
 }
